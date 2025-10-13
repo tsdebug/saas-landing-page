f50ec33582226d153bf4a931ff5a956fe6b65b35
TSdebug <taworkspace01@gmail.com>
2025-10-13 19:30:00 +0530

    Added size property to classes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from "react";
+import { ButtonHTMLAttributes, HTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
 const classes = cva('border h-12 rounded-full px-6 font-medium', {
@@ -6,13 +6,25 @@ const classes = cva('border h-12 rounded-full px-6 font-medium', {
         variant: {
             primary: 'bg-lime-400 text-neutral-950 border-lime-400',
             secondary: 'border-white text-white bg-transparent',
+        },
+        size:{
+            sm:'h-10',
         }
     }
 })
 
-export default function Button(props: { variant: "primary" | "secondary" } & HTMLAttributes<HTMLButtonElement>) {
-    const {variant, className, ...otherProps} = props;
+export default function Button(
+    props: { 
+        variant: "primary" | "secondary" ;
+        size?:'sm'
+    } & ButtonHTMLAttributes<HTMLButtonElement>) {
+    const {variant, className, size, ...otherProps} = props;
     return (
-        <button className={classes({variant, className,})}{...otherProps} />
+        <button 
+        className={classes({
+            variant, 
+            size,
+            className,
+        })}{...otherProps} />
     )
 }
