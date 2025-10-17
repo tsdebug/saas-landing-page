a5b7c870745a9b7428f963b3dc0535a0f8b5240b
TSdebug <taworkspace01@gmail.com>
2025-10-17 23:36:56 +0530

    Added interactivity to feature last part

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -113,8 +113,8 @@ export default function Features() {
                 </div>
                 <div className="mt-8 flex flex-wrap gap-3 justify-center">
                     {features.map((feature) => (
-                        <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 md:px-5 py-1.5 md:py-2 rounded-2xl gap-3 items-center">
-                            <span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex items-center justify-center text-xl">&#10038;</span>
+                        <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 md:px-5 py-1.5 md:py-2 rounded-2xl gap-3 items-center hover:scale-105 transition duration-500 group">
+                            <span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex items-center justify-center text-xl group-hover:rotate-45 transition duration-500">&#10038;</span>
                             <span className="font-medium md:text-lg">{feature}</span>
                         </div>
                     ))}
