c8e093833560ad7e4ee021c7da134a721949906f
TSdebug <taworkspace01@gmail.com>
2025-10-17 22:52:37 +0530

    Added interactivity to featureCard 1

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,6 +3,7 @@ import FeatureCard from "@/components/FeatureCard";
 import avatar1 from '@/assets/images/avatar-ashwin-santiago.jpg';
 import avatar2 from '@/assets/images/avatar-florence-shaw.jpg';
 import avatar3 from '@/assets/images/avatar-lula-meyers.jpg';
+import avatar4 from '@/assets/images/avatar-owen-garcia.jpg';
 import Image from "next/image";
 import Avatar from "@/components/Avatar";
 import Key from "@/components/Key";
@@ -28,37 +29,42 @@ export default function Features() {
                     Where power meets <span className="text-lime-400">simplicity</span>
                 </h2>
                 <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    <FeatureCard 
-                    title="Real-time Collaboration" 
-                    description="Work together seamlessly with conflict-free team editing"
-                    className="md:cols-span-2 lg:cols-span-1"
+                    <FeatureCard
+                        title="Real-time Collaboration"
+                        description="Work together seamlessly with conflict-free team editing"
+                        className="md:cols-span-2 lg:cols-span-1 group"
                     >
                         <div className="aspect-video flex items-center justify-center">
                             <Avatar className="z-40">
-                                <Image 
-                                    src={avatar1} 
+                                <Image
+                                    src={avatar1}
                                     alt="Avatar 1"
                                     className="rounded-full"
                                 />
                             </Avatar>
                             <Avatar className="-ml-6 border-indigo-500 z-30">
-                                <Image 
-                                    src={avatar2} 
+                                <Image
+                                    src={avatar2}
                                     alt="Avatar 2"
                                     className="rounded-full"
                                 />
                             </Avatar>
                             <Avatar className="-ml-6 border-amber-500 z-20">
-                                <Image 
-                                    src={avatar3} 
+                                <Image
+                                    src={avatar3}
                                     alt="Avatar 3"
                                     className="rounded-full"
                                 />
                             </Avatar>
-                            <Avatar className="-ml-6 border-transparent">
-                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
-                                    {Array.from({length:3}).map((_, i) =>(
-                                        <span 
+                            <Avatar className="-ml-6 border-transparent group-hover:border-green-500">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1 relative">
+                                    <Image 
+                                    src={avatar4} 
+                                    alt="Avatar 4" 
+                                    className="absolute size-full rounded-full opacity-0 group-hover:opacity-100 transition"
+                                    />
+                                    {Array.from({ length: 3 }).map((_, i) => (
+                                        <span
                                             className="size-1.5 rounded-full bg-white inline-flex "
                                             key={i}
                                         ></span>
@@ -68,26 +74,26 @@ export default function Features() {
                         </div>
                     </FeatureCard>
 
-                    <FeatureCard  
-                    title="Interacting Prototyping" 
-                    description="Engage your clients with prototypes that react to user actions"
-                    className="md:cols-span-2 lg:cols-span-1"
+                    <FeatureCard
+                        title="Interacting Prototyping"
+                        description="Engage your clients with prototypes that react to user actions"
+                        className="md:cols-span-2 lg:cols-span-1"
                     >
                         <div className="aspect-video flex items-center justify-center">
                             <p className="text-4xl font-extrabold text-white/20 text-center">
-                                We have achived{" "} 
+                                We&apos;ve achived{" "}
                                 <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
                                     incredible
-                                </span>{" "} 
+                                </span>{" "}
                                 growth this year
                             </p>
                         </div>
                     </FeatureCard>
 
-                    <FeatureCard 
-                    title="Keyboard Quick Actions" 
-                    description="Powerful commands to help you create designs more quickly"
-                    className="md:cols-span-2 lg:cols-span-1 lg:col-start-auto md:col-start-2"
+                    <FeatureCard
+                        title="Keyboard Quick Actions"
+                        description="Powerful commands to help you create designs more quickly"
+                        className="md:cols-span-2 lg:cols-span-1 lg:col-start-auto md:col-start-2"
                     >
                         <div className="aspect-video flex items-center justify-center gap-4">
                             <Key className="w-28">shift</Key>
@@ -97,7 +103,7 @@ export default function Features() {
                     </FeatureCard>
                 </div>
                 <div className="mt-8 flex flex-wrap gap-3 justify-center">
-                    {features.map((feature)=>(
+                    {features.map((feature) => (
                         <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 md:px-5 py-1.5 md:py-2 rounded-2xl gap-3 items-center">
                             <span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex items-center justify-center text-xl">&#10038;</span>
                             <span className="font-medium md:text-lg">{feature}</span>
