1967457ae911e8bf6414fb298c7a7bb0a03f8fe7
TSdebug <taworkspace01@gmail.com>
2025-10-18 23:01:11 +0530

    Added animation to the CallToAction  section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,15 +1,49 @@
+'use client';
+
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion"
+import { useEffect, useRef, useState } from "react";
+
 export default function CallToAction() {
+    const [scope, animate] = useAnimate();
+    const animation = useRef<AnimationPlaybackControls>();
+    const [isHovered, setIsHovered] = useState(false);
+    
+    useEffect(()=>{
+        animation.current = animate(
+            scope.current, 
+            {x:'-50%'}, 
+            { duration: 30, ease: 'linear', repeat: Infinity}
+        );
+    }, []);
+
+
+    useEffect(()=>{
+        if (animation.current){
+            if (isHovered){
+                animation.current.speed = 0.5;
+            }
+            else{
+                animation.current.speed = 1;
+            }
+        }
+    }, [isHovered]);
+
     return (
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
-                <div className="flex flex-none gap-16 text-7xl md:text-8xl font-medium">
+                <motion.div
+                ref={scope}
+                className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium group cursor-pointer"
+                onMouseEnter={()=>setIsHovered(true)}
+                onMouseLeave={()=>setIsHovered(false)}
+                >
                     {Array.from({ length: 10 }).map((_, i) => (
                         <div key={i} className="flex items-center gap-16">
                             <span className="text-lime-400">&#10038;</span>
-                            <span>Try it for free</span>
+                            <span className="group-hover:text-lime-400">Try it for free</span>
                         </div>
                     ))}
-                </div>
+                </motion.div>
             </div>
         </section>
 
